9f70db99765d0595694a2155e6b47df9274918dc
Rich Jarvis <rich@uniscen.com>
2019-09-11 06:26:42 -0600

    Added expenseCount and expensesTotal

---
+++ src/components/ExpenseSummary.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import expenseCount from '../selectors/expenseCount';
+import expensesTotal from '../selectors/expenses-total';
+import selectExpenses from '../selectors/expenses';
+import numeral from 'numeral';
+
+export class ExpensesSummary extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2>
+                    Viewing {expenseCount(this.props.expenses)}
+                    {
+                        expenseCount(this.props.expenses) === 1 ? (
+                            ` expense`
+                        ) : (
+                                ` expenses`
+                            )
+                    } totalling {numeral((expensesTotal(this.props.expenses)) / 100).format('$0,0.00')}
+                </h2>
+            </div>
+
+        );
+    }
+}
+
+
+const mapStateToProps = (state) => {
+    return {
+        expenses: selectExpenses(state.expenses, state.filters)
+    }
+};
+
+export default connect(mapStateToProps)(ExpensesSummary);
---
+++ src/tests/components/ExpenseSummary.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import expenses from '../fixtures/expenses';
+import { ExpensesSummary } from '../../components/ExpenseSummary';
+
+test('Should render Expense Summary', () => {
+    const wrapper = shallow(<ExpensesSummary expenses={expenses} />);
+    expect(wrapper).toMatchSnapshot();
+});
+test('Should render Expense Summary', () => {
+    const wrapper = shallow(<ExpensesSummary expenses={[]} />);
+    expect(wrapper).toMatchSnapshot();
+});
