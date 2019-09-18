9dd4715c64604bc189e6ccc7b07b6783a6eaa05f
Rich Jarvis <rich@uniscen.com>
2019-09-18 01:42:48 -0600

    Updated login screen

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,10 +5,10 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
+            <img src="images/bank-icon.png" height="80"></img>
             <h1 className="box-layout__title">Expensify</h1>
-            <p>Keep track of your expenses</p>
+            <p>tracking expenses made easy</p>
             <button className="button" onClick={startLogin}>Login with Google</button>
-
         </div>
     </div>
 );
